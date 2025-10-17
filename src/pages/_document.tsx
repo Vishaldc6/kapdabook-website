c420786a91c844e7e6c9a6aca2c738449d6a9c74
Vishal Chaudhary <vishalchaudharee8@gmail.com>
2025-10-17 20:44:26 +0530

    - update script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const adsenseClient = process.env.NEXT_PUBLIC_ADSENSE_CLIENT
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1094381619101505"
-          crossOrigin="anonymous"></script>
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClient}`}
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body className="antialiased">
         <Main />
